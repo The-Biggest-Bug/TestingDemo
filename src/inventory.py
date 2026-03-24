48515f3b5ab0ea94d8ba314f5809056071752eee
The-Biggest-Bug <smithz3@etsu.edu>
2026-03-24 15:50:47 -0400

    Introduce bug

--- src/inventory.py
+++ src/inventory.py
@@ -25,7 +25,8 @@ def reduce_stock(item_id: str, quantity: int) -> bool:
     if current < quantity:
         return False
     _stock[item_id] = current - quantity
-    return True
+    #return True
+    return _stock[item_id]
 
 
 def reset_stock():
